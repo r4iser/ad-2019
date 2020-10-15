f658e26cdb11baa69da0b5c5dbf46ae0334aa108
Alex <alexsampaio.contato@gmail.com>
2020-10-15 00:59:54 -0300

    Adds dotenv and mongoose
    Mongoose Connection: Ok.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
 
+const db_username = process.env.DB_USER;
+const db_password = process.env.DB_PASS;
+const db_name = process.env.DB_NAME;
+const URI = 'mongodb+srv://' + db_username + ':' + db_password + '@cluster0.re8o9.mongodb.net/' + db_name + '?retryWrites=true&w=majority';
+mongoose.connect(URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(db => console.log("Conectado com sucesso ao banco de dados."))
+  .catch(err => console.log(">> ERROR: ",err));
+
 app.use(express.json());
-app.post('/amigosPush', (req, res) => {
-    return res.json(req.body);
-});
+app.use(routes);
+
 
 
 app.listen(3000);
